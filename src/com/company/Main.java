b722af53bf684f49a770c70616e4187fc27975b4
FlipeFrontDev <felipe.saint@gmail.com>
2021-09-04 14:52:50 -0300

    Adicionando arquivos iniciais

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Then, I realized that, everything came from the water.");
+    }
+}
