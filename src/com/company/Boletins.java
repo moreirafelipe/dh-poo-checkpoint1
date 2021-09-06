a69ee10d2cd180fa0bcc556604b1183254853005
taniarita <taniamendes6523@gmail.com>
2021-09-06 15:00:24 -0300

    atualizando classes

--- src/com/company/Boletins.java
+++ src/com/company/Boletins.java
@@ -1,4 +1,20 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Boletins {
+
+    private String periodo;
+    private ArrayList <Disciplinas> disciplinas;
+
+    public Boletins(String periodo) {
+
+        this.periodo = periodo;
+        this.disciplinas = new ArrayList<>();
+    }
+
+    public void apresentarDados() {
+
+        System.out.println(disciplinas.nota);
+    }
 }
