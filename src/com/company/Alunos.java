b1675befd09f026b0deb2edd49214e62c2ddf7fc
taniarita <taniamendes6523@gmail.com>
2021-09-06 12:26:04 -0300

    Adicionando classes

---
+++ src/com/company/Alunos.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Alunos extends Pessoas {
+
+    private int id_alunos;
+    private String endereco;
+    private Turmas turma;
+    private Boletins boletim;
+    private ArrayList <Boletins> boletins = new ArrayList<>(); //instanciando um novo array que vai conter objetos do tipo Boletins;
+
+    // quando gera um construtor, ele surge com os atributos gerais da superclasse(pai); os atributos da subclasse(filho) devem ser adicionados após;
+
+    public Alunos(String nome, String telefone, LocalDate data_de_nascimento, String categoria, String endereco, Turmas turma) {
+        super(nome, telefone, data_de_nascimento, categoria);
+
+        this.endereco = endereco;
+        this.turma = turma;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ import java.sql.SQLOutput;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Then, I realized that, everything came from the water.");
+
     }
 }
